83bb2b79389eb631a735c7633f07738a94b72d2f
joshimcse <joshimcse@gmail.com>
2019-01-06 11:08:42 +0600

    URI | 1078 - Multiplication Table, Solved using C

---
+++ C/uri1078.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int N, i = 1;
+    scanf("%d", &N);
+
+    while (i <= 10) {
+        printf("%d x %d = %d\n", i, N, i * N);
+        i++;
+    }
+    return 0;
+}
