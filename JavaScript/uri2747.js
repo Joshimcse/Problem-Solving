40824d560cc6ca7f851312dd8ab0178989aa3af0
joshimcse <joshimcse@gmail.com>
2019-01-15 12:27:22 +0600

    URI | 2747 - Output 1, Solved using JavaScript

---
+++ JavaScript/uri2747.js
@@ -0,0 +1,18 @@
+let type1 = '';
+let type2 = '';
+
+for (let i = 0; i < 39; i++) {
+    type1 += '-';
+    if (i == 0 || i == 38)
+        type2 += '|';
+    else
+        type2 += ' ';
+}
+
+console.log(type1);
+console.log(type2);
+console.log(type2);
+console.log(type2);
+console.log(type2);
+console.log(type2);
+console.log(type1);
