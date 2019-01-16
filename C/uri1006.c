b360f67fed238532c4e4c718ec2b9af974497023
joshimcse <joshimcse@gmail.com>
2019-01-16 09:51:01 +0600

    URI | 1005 - Average 2, Solved using C

---
+++ C/uri1006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float A, B, C;
+
+    scanf("%f", &A);
+    scanf("%f", &B);
+    scanf("%f", &C);
+
+    float MEDIA = A / 10 * 2 + B / 10 * 3 + C / 10 * 5;
+
+    printf("MEDIA = %.1f\n", MEDIA);
+
+    return 0;
+}
