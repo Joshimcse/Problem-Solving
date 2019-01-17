f5e57fb1f2642f08d0beb6b12dc05f74b715e2c1
joshimcse <joshimcse@gmail.com>
2019-01-17 23:37:44 +0600

    URI | 1020 - Age in Days, Solved using C

---
+++ C/uri1020.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int day, month, year;
+    scanf("%d", &day);
+    year = day / 365;
+    day = day % 365;
+    month = day / 30;
+    day = day % 30;
+
+    printf("%d ano(s)\n", year);
+    printf("%d mes(es)\n", month);
+    printf("%d dia(s)\n", day);
+
+    return 0;
+}
