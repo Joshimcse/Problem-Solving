626af594741598c41c09de2f0fb1ba9b41f43fba
joshimcse <joshimcse@gmail.com>
2019-03-20 15:49:38 +0600

    URI | 1072 (Interval 2) Solved using Java

--- Java/URI1072.java
+++ Java/URI1072.java
@@ -2,7 +2,6 @@ import java.io.IOException;
 import java.util.Scanner; 
 
 public class Main {
- 
     public static void main(String[] args) throws IOException {
         Scanner scr = new Scanner(System.in);
         int n = scr.nextInt();
